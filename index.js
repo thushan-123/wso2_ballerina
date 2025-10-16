915c2cfb86dddc1a43b7d6b9f2ec8a4ecc1a4c4f
thushan-123 <thushanmadusanka456@gmail.com>
2025-10-16 22:09:42 +0530

    added middleware in index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const logger = require("./middleware/logger");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Student API 🚀");
