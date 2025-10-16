96ca5c276e4cb74ca7a0a43f6d73f86612d33b59
thushan-123 <thushanmadusanka456@gmail.com>
2025-10-16 22:18:02 +0530

    add student route

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("./middleware/logger");
+const studentRoutes = require("./routes/students");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(logger);
+
+app.use("/api/students", studentRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Student API 🚀");
