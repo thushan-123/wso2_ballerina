58011bac400e079d6edd097a8efb1f59915d0eb7
thushan-123 <thushanmadusanka456@gmail.com>
2025-10-16 22:13:15 +0530

    read tha json file

--- routes/students.js
+++ routes/students.js
@@ -4,3 +4,9 @@ const path = require("path");
 
 const router = express.Router();
 const filePath = path.join(__dirname, "../data/students.json");
+
+
+const readData = () => {
+  const data = fs.readFileSync(filePath, "utf8");
+  return JSON.parse(data);
+};
